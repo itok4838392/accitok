93fc484d940e5594a2988f74bc1ac04486a572b9
itok4838392 <82092983+itok4838392@users.noreply.github.com>
2021-12-24 15:08:41 +0100

    Update new.py

--- new.py
+++ new.py
@@ -4,6 +4,7 @@ deviceid="17A7F633BD2668F5C58AC8FF2E0DDB52FE738274220CF20A44FBD5C421B24F47D483B6
 emaill="Itokistback@gmail.com"
 passwordd="Diana2006"
 custompwd="fakeaccpwd"
+private="e529476c-ffae-41a5-aa8a-d701a42ea63f"
 url="https://ElatedSlipperyArchive.crimsondead.repl.co"
 chatlink="http://aminoapps.com/p/c9xmj7"
 nickname="Itok"
