ddf72b936b5ae794611ae998367079ece3d78fa6
itok4838392 <82092983+itok4838392@users.noreply.github.com>
2021-12-24 13:50:22 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,5 +1,5 @@
-key='e36bb985-e2f1-43fd-b85e-e4569be5d05b'
-app_name="accgen123"
+key='890547c9-a936-4fe0-94a6-bed0d31e347f'
+app_name="accgen124"
 deviceid="17A7F633BD2668F5C58AC8FF2E0DDB52FE738274220CF20A44FBD5C421B24F47D483B642ED67AE381E"
 emaill="e8bf0gjt9@xojxe.com"
 passwordd="tempmail"
