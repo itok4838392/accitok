7a8f650e3614eb97c855a171b8bc587cea012b70
itok4838392 <82092983+itok4838392@users.noreply.github.com>
2021-12-24 14:18:04 +0100

    Update new.py

--- new.py
+++ new.py
@@ -1,8 +1,8 @@
 key='890547c9-a936-4fe0-94a6-bed0d31e347f'
-app_name="accgen124"
+app_name="accitok"
 deviceid="17A7F633BD2668F5C58AC8FF2E0DDB52FE738274220CF20A44FBD5C421B24F47D483B642ED67AE381E"
-emaill="e8bf0gjt9@xojxe.com"
-passwordd="symeon2006"
+emaill="Itokistback@gmail.com"
+passwordd="Diana2006"
 custompwd="fakeaccpwd"
 url="https://ElatedSlipperyArchive.crimsondead.repl.co"
 chatlink="http://aminoapps.com/p/c9xmj7"
